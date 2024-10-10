6ee7061acd2ec338a7d3ab148f3d210085467c6b
klhaug <kristian@snougle.no>
2024-10-10 16:01:50 +0200

    working on the submit API

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require("cors");
 const path = require('path');
-const { default: test } = require('node:test');
+// const { default: test } = require('node:test');
 
 const app = express();
 
@@ -9,20 +10,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const testDatabase = [
     {
-        Navn: "Alocasia",
-        Slekt: "Aracea",
-        Vann: "50ml / En gang i uken",
-        Giftig: "Ja",
-        Beskrivelse: "Alocasia er en tropisk plante fra Araceae-familien, kjent for sine store, pilformede blader med markante årer. Bladene har ofte en blank overflate og kommer i varierte farger, fra mørkegrønn til sølvgrå. Alocasia trives i varme og fuktige omgivelser, men krever indirekte lys og jevn vanning. Planten er populær som stueplante på grunn av sitt unike, eksotiske utseende. Den kan være litt krevende å pleie, men belønner med imponerende løvverk når den får riktig stell.",
-        Bilde: 'http://localhost:3000/images/IMG_5590.JPG'
+        navn: "Alocasia",
+        slekt: "Aracea",
+        vann: "50ml / En gang i uken",
+        giftig: "Ja",
+        beskrivelse: "Alocasia er en tropisk plante fra Araceae-familien, kjent for sine store, pilformede blader med markante årer. Bladene har ofte en blank overflate og kommer i varierte farger, fra mørkegrønn til sølvgrå. Alocasia trives i varme og fuktige omgivelser, men krever indirekte lys og jevn vanning. Planten er populær som stueplante på grunn av sitt unike, eksotiske utseende. Den kan være litt krevende å pleie, men belønner med imponerende løvverk når den får riktig stell. ",
+        bilde: 'http://localhost:3000/images/IMG_5590.JPG'
         
     }
 ]
 app.use(cors())
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
-    res.json(testDatabase[0])
+    res.json(testDatabase[testDatabase.length - 1])
 });
+
+app.post("/submit", (req, res) => {
+    testDatabase.push(req.body)
+    console.log(testDatabase)
+    res.json(testDatabase)
+    // .catch(err => res.status(400).json(err))
+})
 
 // app.get("/plants", (req, res) => {
 //     res.json(testDatabase[0])
